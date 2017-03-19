4c40aae27ab9881b3fcf087b73ebe76c1c3c354c
kan1shka9 <kanishka.s9@gmail.com>
2017-03-19 11:07:33 -0700

    Mongoose

--- prod/users/test/test_helper.js
+++ prod/users/test/test_helper.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
+// Use the ES6 implementation of the Promise
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
   .once('open', () => console.log('Good to go!'))
   .on('error', (error) => {
     console.warn('Warning', error);
   });
-
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
