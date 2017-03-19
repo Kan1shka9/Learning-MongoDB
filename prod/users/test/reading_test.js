068a16f5527c4f11abd74de3eec404c312f8b998
kan1shka9 <kanishka.s9@gmail.com>
2017-03-19 11:20:47 -0700

    Mongoose

---
+++ prod/users/test/reading_test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the database', () => {
+  it('finds all users with the name of Joe', ()=> {
+    
+  });
+});
--- prod/users/test/test_helper.js
+++ prod/users/test/test_helper.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
-// Use the ES6 implementation of the Promise
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
